8a51c60c87d3d9b74b408ffcc9bd70b89c00d3ce
ibeholder <35081907+ibeholder@users.noreply.github.com>
2024-07-19 13:38:44 -0600

    Update Get-Smartcard.ps1

--- Get-Smartcard.ps1
+++ Get-Smartcard.ps1
@@ -1,8 +1,9 @@
 Function Get-Smartcard {
-    $raw = certutil -scinfo -silent | Out-String
-    $split = $raw -Split "--------------===========================--------------"
+    $raw = certutil -scinfo -silent| Out-String
     $nocard = $raw|select-string -pattern 'SCARD_STATE_EMPTY'
     $nocard 
+    $split = $raw -Split "--------------===========================--------------"
+
     $count = $split.Length
     $i = 1
     $certs = @()
@@ -14,7 +15,7 @@ Function Get-Smartcard {
             $cleaned = $top[0].split([Environment]::NewLine) -Replace '^\s+','' -Replace '^---\s+',''
             $filtered = $cleaned | Select-String -Pattern ' Certificate ','^Reader:','Card:','Key Container =','Provider =','Serial Number','Subject:','NotBefore','NotAfter'
             $table = $filtered -Replace ': ','=' -Replace ' = ','=' -Replace '================','' -Replace 'cate ','cate=' -Replace ' \[Default Container\]','' | ConvertFrom-StringData
-            
+          
             if ($table.Subject.Count -eq 1) {
                 $cert = [PSCustomObject]@{
                     Certificate     = $table.Certificate
@@ -24,9 +25,9 @@ Function Get-Smartcard {
                     Reader          = $table.Reader
                     Card            = $table.Card
                     Provider        = $table.Provider
-                    'Key Container' = $table.'Key Container'
-                    'Serial Number' = $table.'Serial Number'
-                    San = (certutil -v -silent -store -user My $table.Certificate|select-string -pattern '@') -Replace " ",''
+                    KeyContainer = $table.'Key Container'
+                    SerialNumber = $table.'Serial Number'
+                    San = (certutil -v -silent -store -user My $table.'Serial Number'|select-string -pattern 'Principal') -Replace " ",'' -Replace "PrincipalName=",''
                 }
                 $certs += $cert
             }
@@ -36,3 +37,5 @@ Function Get-Smartcard {
     }
     Return $certs
 }
+
+Get-Smartcard| Out-GridView
